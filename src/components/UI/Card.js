6de57bc6fab64b1ddfee9e41d305526e8eb44b20
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2022-09-24 22:52:00 -0300

    Changing function syntax to arrow functions

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import "./Card.css";
 
-function Card(props) {
+const Cards = props => {
     const classes = 'card ' + props.className; // Tudo que recebermos como nome de classe no componentes sera concatenado nesta string
     return (
         <div className={classes}>
